c2d40044107081ce5e57d6f577296d02840f2a54
kokutoupan <kokutou2004@gmail.com>
2022-11-09 13:28:32 +0900

    課題29−31

--- Scripts/struct5.c
+++ Scripts/struct5.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <string.h>/* strcpyに必要*/
 #include <stdlib.h>/* mallocに必要*/
+#include <string.h>
 
 struct node
 {
@@ -13,9 +14,12 @@ struct node
 struct node *add(char *, struct node *);
 void show(struct node *);
 void free_list(struct node *);
-void insert(int n, char *data, struct node **head);
 
+void insert(int n, char *data, struct node **head);
 void insert3(int n, char *data, struct node **pHead);
+
+void delete (int n, struct node **pHead);
+void delete_name(char *str, struct node **pHead);
 
 int main(void)
 {
@@ -25,30 +29,42 @@ int main(void)
     char c;
     int stdinFlag = stdin->_flags;
 
-    //printf("%x\n",stdin->_flags);
+    // printf("%x\n",stdin->_flags);
     /* 先頭ポインタをNULLに*/
     while (scanf("%s", data) != EOF)
     {
         stdinFlag = stdin->_flags;
         head = add(data, head);
-        //printf("%x\n",stdin->_flags);
+        // printf("%x\n",stdin->_flags);
     }
     show(head);
-    //fflush(stdin);
-    //getchar();
-    //while(getchar()!= 1);
-    //fscanf(stdin,"%s",data);
-    //while (c != '\n') c = getchar();
+    // fflush(stdin);
+    // getchar();
+    // while(getchar()!= 1);
+    // fscanf(stdin,"%s",data);
+    // while (c != '\n') c = getchar();
     stdin->_flags = stdinFlag; //これでctrl-Dを押す前のstdinのフラグを上書きする
-    //printf("%x",stdin->_flags);
+    // printf("%x",stdin->_flags);
 
-    printf("挿入するデータ: ");
-    scanf("%s", data);
-      
-    printf("何番目の後に: ");
-    scanf("%d", &n);
-    insert3(n, data, &head); /* ノードの挿入*/
-    show(head);            /* 挿入後のノードの表示*/
+    // printf("挿入するデータ: ");
+    // scanf("%s", data);
+
+    // printf("何番目の後に: ");
+    // scanf("%d", &n);
+    // insert3(n, data, &head); /* ノードの挿入*/
+    // show(head);              /* 挿入後のノードの表示*/
+
+    // /* 要素の削除*/
+    // printf("何番目を削除しますか\n");
+    // scanf("%d", &n);
+    // delete (n, &head);
+    // show(head);
+
+    printf("どのデータを削除しますか\n");
+    scanf("%s",data);
+    delete_name(data,&head);
+    show(head);
+
     free_list(head);
 
     return 0;
@@ -90,16 +106,17 @@ void free_list(struct node *p)
 /* リストにノードを挿入する関数*/
 void insert(int n, char *data, struct node **head)
 {
-    struct node* p = *head;
+    struct node *p = *head;
     int i;
     struct node *x; /* 挿入するノードの準備*/
     x = malloc(sizeof(struct node));
-    strcpy(x->name, data); 
-    
-    if(n == 0){
-      *head = x;
-      x->next = p;
-      return;
+    strcpy(x->name, data);
+
+    if (n == 0)
+    {
+        *head = x;
+        x->next = p;
+        return;
     }
     /* 挿入する位置の手前までノードをたどる*/
     for (i = 0; i < n - 1; i++)
@@ -110,10 +127,37 @@ void insert(int n, char *data, struct node **head)
 
 void insert3(int n, char *data, struct node **pHead)
 {
-    struct node* p = *pHead;
-    for(int i=0;i < n;i++){
-      pHead = &(*pHead)->next;
+    for (int i = 0; i < n; i++)
+    {
+        pHead = &(*pHead)->next;
     }
-    *pHead = add(data,*pHead);
+    *pHead = add(data, *pHead);
+}
 
+/* n番目の要素を削除する関数*/
+void delete (int n, struct node **pHead)
+{
+    for (int i = 0; i < n - 1; ++i)
+    {
+        pHead = &(*pHead)->next;
+    }
+    struct node *tmp = *pHead;
+    *pHead = (*pHead)->next;
+    free(tmp);
+}
+
+void delete_name(char *str, struct node **pHead)
+{
+    while (*pHead != NULL)
+    {
+        if (!strcmp(str, (*pHead)->name))
+        {
+            break;
+        }
+        pHead = &(*pHead)->next;
+    }
+
+    struct node *tmp = *pHead;
+    *pHead = (*pHead)->next;
+    free(tmp);
 }
