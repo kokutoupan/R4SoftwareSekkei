95130a7e8c3322a7d361f51f6ed4415709f668e3
kokutoupan <kokutou2004@gmail.com>
2023-01-25 14:21:29 +0900

    修正

--- Scripts/c++/stack/MyStack.hpp
+++ Scripts/c++/stack/MyStack.hpp
@@ -12,7 +12,7 @@ struct POINT
 };
 
 template <class Char> // POINTの出力演算子のオーバーロード
-inline std::basic_ostream<Char>& operator <<(std::basic_ostream<Char>& os, const POINT& v)
+inline std::basic_ostream<Char> &operator<<(std::basic_ostream<Char> &os, const POINT &v)
 {
     return os << Char('(') << v.x << Char(',') << v.y << Char(')');
 }
@@ -26,67 +26,80 @@ class MyStack
     T *capacity_last;
 
     int capacity_size;
-    int size;
+    int mSize;
+
 public:
     MyStack();
     MyStack(const unsigned int n);
     ~MyStack();
 
-    bool Pop(T& out);
-    bool Push(const T& _pos);
+    bool Pop(T &out);
+    T Pop(bool &ret);
+    bool Push(const T &_pos);
 
     bool IsEmpty() const;
     bool IsFull() const;
     void Show() const;
+
+    int size() const;
+
+    int remain() const;
 };
 
-
 //-----------------------------------------------------------------------------
-//実装
+// 実装
 //-----------------------------------------------------------------------------
 
-template<class T>
+template <class T>
 MyStack<T>::MyStack()
 {
-    size = 0;
+    mSize = 0;
     capacity_size = 1;
     data = new T[capacity_size];
     head = data;
     capacity_last = data + capacity_size;
 }
 
-template<class T>
+template <class T>
 MyStack<T>::MyStack(const unsigned int n)
 {
-    size = 0;
+    mSize = 0;
     capacity_size = n;
     data = new T[capacity_size];
     head = data;
     capacity_last = data + capacity_size;
 }
-template<class T>
+template <class T>
 MyStack<T>::~MyStack()
 {
     delete[] data;
 }
 
 template <class T>
-bool MyStack<T>::Pop(T& out)
+bool MyStack<T>::Pop(T &out)
 {
-    if(this->IsEmpty()){
-        return false;
-    }
-    head--;
-    size--;
-    out = *head;
-    return true;
+    bool ret;
+    out = this->Pop(ret);
+    return ret;
 }
 
+template <class T>
+T MyStack<T>::Pop(bool &ret){
+    if (this->IsEmpty())
+    {
+        ret = false;
+        return T();
+    }
+    ret = true;
+    head--;
+    mSize--;
+    return *head;
+}
 
 template <class T>
 bool MyStack<T>::Push(const T &_pos)
 {
-    //std::cout << "aaa" << std::endl;
+    // std::cout << "aaa" << std::endl;
     if (this->IsFull())
     {
         T *tmp = data;
@@ -96,7 +109,7 @@ bool MyStack<T>::Push(const T &_pos)
         data = new T[capacity_size];
         std::copy(tmp, head, data);
 
-        head = data + size;
+        head = data + mSize;
         capacity_last = data + capacity_size;
 
         // std::cout << "(" << data->x << ',' << data->y << ")\n";
@@ -105,29 +118,43 @@ bool MyStack<T>::Push(const T &_pos)
 
     *head = _pos;
     head++;
-    size++;
+    mSize++;
     return true;
 }
 
-template<class T>
-bool MyStack<T>::IsEmpty() const{
-    return size ? false:true;
+template <class T>
+bool MyStack<T>::IsEmpty() const
+{
+    return mSize ? false : true;
 }
-template<class T>
-bool MyStack<T>::IsFull() const{
+template <class T>
+bool MyStack<T>::IsFull() const
+{
     return capacity_last == head;
 }
 
-template<class T>
-void MyStack<T>::Show() const{
-    T* i = head -1;
+template <class T>
+void MyStack<T>::Show() const
+{
+    T *i = head - 1;
     std::cout << "\n";
     while (i >= data)
     {
-        std::cout << "|"<< *i << "|\n";
+        std::cout << "|" << *i << "|\n";
         i--;
     }
     std::cout << "-------------------------------\n";
-    
 }
+
+template <class T>
+int MyStack<T>::size() const
+{
+    return this->capacity_size;
+}
+template <class T>
+int MyStack<T>::remain() const
+{
+    return this->capacity_size - this->mSize;
+}
+
 #endif
--- Scripts/c++/stack/main.cpp
+++ Scripts/c++/stack/main.cpp
@@ -3,7 +3,8 @@
 
 #include "MyStack.hpp"
 
-int main(){
+int main()
+{
     MyStack<POINT> stack;
     bool ret;
     bool loop = true;
@@ -13,41 +14,51 @@ int main(){
     std::random_device seed_gen;
     std::mt19937 engine(seed_gen());
 
-    std::uniform_int_distribution<> dist(-100,100);
+    std::uniform_int_distribution<> dist(-100, 100);
 
-    while(loop){
+    while (loop)
+    {
         std::cout << "0) 終了\n";
         std::cout << "1) PUSH\n";
         std::cout << "2) POP\n";
+        std::cout << "3) size\n";
+        std::cout << "4) あとはいる量\n";
 
         std::cout << "\nどの操作をしますか？:";
         std::cin >> res;
 
-        switch(res){
-            case 0:
-                goto loopEnd;
-            case 1:
-                ret = stack.Push(POINT(dist(engine),dist(engine)));
-                if(!ret){
-                    std::cout << "Stack is Error\n";
-                }
-                break;
-            case 2:
-                ret = stack.Pop(p);
+        switch (res)
+        {
+        case 0:
+            goto loopEnd;
+        case 1:
+            ret = stack.Push(POINT(dist(engine), dist(engine)));
+            if (!ret)
+            {
+                std::cout << "Stack is Error\n";
+            }
+            break;
+        case 2:
+            ret = stack.Pop(p);
 
-                if(ret){
-                    std::cout << "pop: >>" << p;
-                }
-                else{
-                    std::cout << "Stack is Empty\n";
-                }
+            if (ret)
+            {
+                std::cout << "pop: >>" << p;
+            }
+            else
+            {
+                std::cout << "Stack is Empty\n";
+            }
 
-                break;
-            case 3:
-                stack.Show();
-                break;
-            default:
-                std::cout << "ちゃんと入力して\n";
+            break;
+        case 3:
+            std::cout << stack.size() << "\n";
+            break;
+        case 4:
+            std::cout << stack.remain() << "\n";
+            break;
+        default:
+            std::cout << "ちゃんと入力して\n";
         }
         stack.Show();
     }
